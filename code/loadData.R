7907ffacb6c68e0e4bc3bde7e17a87876c40387b
Ivan Domenzain <ivand@chalmers.se>
2019-08-21 16:20:12 +0200

    feat: add loadData function

---
+++ code/loadData.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env Rscript
+# Functions for loading and minor formatting of all different datasets used in this study
+# Ivan Domenzain
+
+## @knitr loadKcats
+#setwd('/Users/ivand/Documents/GitHub/Kcat_analysis/code')
+kcats <- read.csv(file = '../data/max_KCAT.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+
+## @knitr load_Sp_Act
+Sp_Act <- read.csv(file = '../data/max_SA.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+
+## @knitr loadMWs
+MWs <- read.csv(file = '../data/max_MW.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+
+## @knitr loadKMs
+KMs <- read.csv(file = '../data/min_KM.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
+
+## @knitr loadKEGG_enzData
+enzData <- read.csv(file = '../data/enzyme.txt', sep = '\t', header = FALSE, stringsAsFactors = FALSE)
